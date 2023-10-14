d1fe314cbcda99e8b14710e7e1cdbfaa44edbe0f
omar <mr.omardiab@outlook.com>
2023-10-13 21:32:19 -0500

    test

---
+++ little-lemon/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>home page</p>
+      <p>testing</p>
+    </div>
+  );
+}
+
+export default App;
